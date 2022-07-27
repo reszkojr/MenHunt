3159d94f33a28199b51a47159b6367dae40c390c
reszkojr <s.reszkojr@gmail.com>
2022-07-27 18:03:29 -0300

    Speedrunner command is now functional

--- src/me/santres/menhunt/Commands.java
+++ src/me/santres/menhunt/Commands.java
@@ -1,22 +1,42 @@
 package me.santres.menhunt;
 
+import me.santres.menhunt.entities.Speedrunner;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public class Speedrunner implements CommandExecutor {
+public class Commands implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
         if (cmd.getName().equals("speedrunner")) {
             if (args.length == 2) {
+                Player speedrunnerCandidate = Bukkit.getPlayer(args[1]);
+                Speedrunner speedrunner = Speedrunner.getInstance();
+
+                if (speedrunnerCandidate == null) {
+                    sender.sendMessage(ChatColor.RED + "Player does not exist.");
+                    return false;
+                }
+
                 switch (args[0]) {
                     case "add" -> {
-
+                        if (speedrunner.get() == speedrunnerCandidate) {
+                            sender.sendMessage(ChatColor.AQUA + speedrunnerCandidate.getName() + " already is speedrunner!");
+                        }
+                        speedrunner.set(speedrunnerCandidate);
+                        sender.sendMessage(ChatColor.GREEN + speedrunnerCandidate.getName() + " is now speedrunner!");
                     }
 
                     case "remove" -> {
-
+                        if (speedrunner.get() == null) {
+                            sender.sendMessage(ChatColor.RED + "No speedrunner was set yet!");
+                        }
+                        sender.sendMessage(ChatColor.GREEN + speedrunnerCandidate.getName() +" is not speedrunner anymore.");
+                        speedrunner.set(null);
                     }
                 }
             } else {
